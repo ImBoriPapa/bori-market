6eb4d0bebb8e417e23cfef6e7055af0be6014087
cszzang89@gamail.com <cszzang89@gmail.com>
2022-12-14 09:22:13 +0900

    modify

--- address-service/src/main/java/com/bmarket/addressservice/router/AddressRouter.java
+++ address-service/src/main/java/com/bmarket/addressservice/router/AddressRouter.java
@@ -2,15 +2,18 @@ package com.bmarket.addressservice.router;
 
 import com.bmarket.addressservice.handler.AddressHandler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.web.reactive.function.server.*;
 
+
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 
 @Configuration
+@Slf4j
 public class AddressRouter {
     /**
      * URL 맵핑
@@ -19,7 +22,7 @@ public class AddressRouter {
     public RouterFunction<ServerResponse> search(AddressHandler addressHandler) {
         return route()
                 .GET("/internal/address", addressHandler::searchByTown)
-                .GET("/address/search-form",addressHandler::searchForm)
+                .GET("/address/search-form", addressHandler::searchForm)
                 .build();
     }
 }
